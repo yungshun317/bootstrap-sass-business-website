6b7740100fe75b523801b3924acf30c98a48c491
yungshun317 <seraph317@gmail.com>
2021-08-03 09:43:44 +0800

    [FEAT] Create the `App` component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "../public/scss/main.scss";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+
+render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    rootElement
+);
