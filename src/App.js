88ab6fd9470f31736c28ec985b454e33b89b9621
yungshun317 <seraph317@gmail.com>
2021-08-05 11:16:26 +0800

    [FEAT] Create all section headers

--- src/App.js
+++ src/App.js
@@ -19,14 +19,104 @@ export default function App() {
                 </Row>
             </Container>
             {/* [3] Service - Development Section 1 */}
+            <Container>
+                <Row>
+                    <Col lg={12}>
+                        <div className="text-center">
+                            <h6>ABOUT</h6>
+                            <h4>Lorem Ipsum</h4>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             {/* [4] Service - Development Section 2 */}
+            <Container>
+                <Row>
+                    <Col lg={12}>
+                        <div className="text-center">
+                            <h6>ABOUT</h6>
+                            <h4>Lorem Ipsum</h4>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             {/* [5] Service - Development Section 3 */}
+            <Container>
+                <Row>
+                    <Col lg={12}>
+                        <div className="text-center">
+                            <h6>ABOUT</h6>
+                            <h4>Lorem Ipsum</h4>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             {/* [6] Service - Design Section 1 */}
+            <Container>
+                <Row>
+                    <Col lg={12}>
+                        <div className="text-center">
+                            <h6>ABOUT</h6>
+                            <h4>Lorem Ipsum</h4>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             {/* [7] Service - Design Section 2 */}
+            <Container>
+                <Row>
+                    <Col lg={12}>
+                        <div className="text-center">
+                            <h6>ABOUT</h6>
+                            <h4>Lorem Ipsum</h4>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             {/* [8] Service - Design Section 3 */}
+            <Container>
+                <Row>
+                    <Col lg={12}>
+                        <div className="text-center">
+                            <h6>ABOUT</h6>
+                            <h4>Lorem Ipsum</h4>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             {/* [9] Team */}
+            <Container>
+                <Row>
+                    <Col lg={12}>
+                        <div className="text-center">
+                            <h6>ABOUT</h6>
+                            <h4>Lorem Ipsum</h4>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             {/* [10] Pricing Table */}
+            <Container>
+                <Row>
+                    <Col lg={12}>
+                        <div className="text-center">
+                            <h6>ABOUT</h6>
+                            <h4>Lorem Ipsum</h4>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             {/* [11] Form */}
+            <Container>
+                <Row>
+                    <Col lg={12}>
+                        <div className="text-center">
+                            <h6>ABOUT</h6>
+                            <h4>Lorem Ipsum</h4>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
