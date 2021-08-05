2956957a846600ef98c57fa22a9137319d325cf2
yungshun317 <seraph317@gmail.com>
2021-08-05 10:58:08 +0800

    [DOCS] Write comments in React and JSX

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import { Container, Row, Col } from "react-bootstrap";
 export default function App() {
     return (
         <>
+            {/* [1] Hero Video */}
             <div className="bg-light" style={{height:"768px"}}>
             </div>
+            {/* [2] About */}
+            {/* [3] Service - Development Section 1 */}
+            {/* [4] Service - Development Section 2 */}
+            {/* [5] Service - Development Section 3 */}
+            {/* [6] Service - Design Section 1 */}
+            {/* [7] Service - Design Section 2 */}
+            {/* [8] Service - Design Section 3 */}
+            {/* [9] Team */}
+            {/* [10] Pricing Table */}
+            {/* [11] Form */}
         </>
     );
 }
