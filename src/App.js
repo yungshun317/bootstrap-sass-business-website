89c99847460ba24f679b8d171e360319a21a9506
yungshun317 <seraph317@gmail.com>
2021-08-25 14:40:32 +0800

    [FEAT] Add `?autoplay=1&mute=1` to the YouTube URL string

--- src/App.js
+++ src/App.js
@@ -130,7 +130,7 @@ export default function App() {
 
             {/* [1] Hero Video */}
             <div className="bg-light" style={{position:"relative",height:"0",overflow:"hidden",paddingBottom:"56.25%"}}>
-                <iframe style={{position:"absolute",top:"0",left:"0",width:"100%",height:"100%"}} src="https://www.youtube.com/embed/rTtSwHkpRC8"
+                <iframe style={{position:"absolute",top:"0",left:"0",width:"100%",height:"100%"}} src="https://www.youtube.com/embed/rTtSwHkpRC8?autoplay=1&mute=1"
                         title="YouTube video player" frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen>
